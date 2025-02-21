74f5b295a90e2cd0bce208810e298f5d866b17e1
alexchen <alexchen.tech@gmail.com>
2025-02-21 16:10:09 +0800

    feat: add sink mysql demo

--- flink-demo/src/main/java/tech/alexchen/daydayup/flink/demo/source/SocketSourceDemo.java
+++ flink-demo/src/main/java/tech/alexchen/daydayup/flink/demo/source/SocketSourceDemo.java
@@ -10,10 +10,8 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class SocketSourceDemo {
 
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment
-                .getExecutionEnvironment();
-        DataStreamSource<String> dataStreamSource =
-                env.socketTextStream("localhost", 9999);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> dataStreamSource = env.socketTextStream("localhost", 9999);
         dataStreamSource.print();
         env.execute();
     }
--- flink-demo/src/main/java/tech/alexchen/daydayup/flink/demo/split/ProcessDemo.java
+++ flink-demo/src/main/java/tech/alexchen/daydayup/flink/demo/split/ProcessDemo.java
@@ -1,6 +1,5 @@
 package tech.alexchen.daydayup.flink.demo.split;
 
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.streaming.api.datastream.SideOutputDataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -9,6 +8,7 @@ import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 import tech.alexchen.daydayup.flink.demo.bean.WaterSensor;
+import tech.alexchen.daydayup.flink.demo.functions.StringToWaterSensorMapFunction;
 
 /**
  * process 分流
@@ -18,8 +18,8 @@ import tech.alexchen.daydayup.flink.demo.bean.WaterSensor;
  */
 public class ProcessDemo {
 
-    public static OutputTag<WaterSensor> outputTagS1 = new OutputTag<WaterSensor>("s1", Types.POJO(WaterSensor.class));
-    public static OutputTag<WaterSensor> outputTagS2 = new OutputTag<WaterSensor>("s2", Types.POJO(WaterSensor.class));
+    public static OutputTag<WaterSensor> outputTagS1 = new OutputTag<>("s1", Types.POJO(WaterSensor.class));
+    public static OutputTag<WaterSensor> outputTagS2 = new OutputTag<>("s2", Types.POJO(WaterSensor.class));
 
 
     public static void main(String[] args) throws Exception {
@@ -27,18 +27,12 @@ public class ProcessDemo {
         env.setParallelism(1);
 
         SingleOutputStreamOperator<WaterSensor> streamOperator = env.socketTextStream("localhost", 9999)
-                .map(new MapFunction<String, WaterSensor>() {
-                    @Override
-                    public WaterSensor map(String value) throws Exception {
-                        String[] split = value.split(",");
-                        return new WaterSensor(split[0], Long.valueOf(split[1]), Integer.valueOf(split[2]));
-                    }
-                });
+                .map(new StringToWaterSensorMapFunction());
 
         // process 返回的是主流的数据
         SingleOutputStreamOperator<WaterSensor> process = streamOperator.process(new CustomProcess());
 
-        OutputTag<WaterSensor> tag1 = new OutputTag<WaterSensor>("s1", Types.POJO(WaterSensor.class));
+        OutputTag<WaterSensor> tag1 = new OutputTag<>("s1", Types.POJO(WaterSensor.class));
         SideOutputDataStream<WaterSensor> s1 = process.getSideOutput(tag1);
         SideOutputDataStream<WaterSensor> s2 = process.getSideOutput(outputTagS2);
 
@@ -53,7 +47,7 @@ public class ProcessDemo {
     public static class CustomProcess extends ProcessFunction<WaterSensor, WaterSensor> {
 
         @Override
-        public void processElement(WaterSensor value, ProcessFunction<WaterSensor, WaterSensor>.Context ctx, Collector<WaterSensor> out) throws Exception {
+        public void processElement(WaterSensor value, ProcessFunction<WaterSensor, WaterSensor>.Context ctx, Collector<WaterSensor> out) {
             String id = value.getId();
             if ("s1".equals(id)) {
                 ctx.output(outputTagS1, value);
